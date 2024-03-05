11acea130a4cfad771526cc801c4cc4bf8fdd6cb
Akshat Gupta <guptaakshat370@gmai.com>
2024-03-05 19:09:59 +0530

    import all pages

---
+++ src/pages/index.js
@@ -0,0 +1,18 @@
+import AddPost from "./AddPost";
+import AllPosts from "./AllPosts";
+import Home from "./Home";
+import Login from "./Login";
+import Signup from "./Signup";
+import EditPost from "./EditPost";
+import Post from "./Post";
+
+
+export {
+    AddPost,
+    AllPosts,
+    Home,
+    Login,
+    Signup,
+    EditPost,
+    Post    
+}
