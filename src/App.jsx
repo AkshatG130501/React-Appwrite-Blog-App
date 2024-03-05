95b4baa64f5d436eb88f643058ad9ebe6cdee0e9
Akshat Gupta <guptaakshat370@gmai.com>
2024-03-05 11:48:49 +0530

    fix: imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,38 @@
 import './App.css'
+import React, { useEffect, useState } from 'react'
+import { useDispatch } from 'react-redux'
+import authService from "./appwrite/auth"
+import {login, logout} from "./features/authentication/authSlice"
+import Header from './components/Header/Header'
+import Footer from './components/Footer/Footer'
 
 function App() {
-  console.log(import.meta.env.VITE_APPWRITE_URL);
-  return (
-    <>
-      <h1>React App</h1>
-    </>
-  )
+  const [loading, setLoading] = useState(false);
+  const dispatch = useDispatch();
+
+  useEffect(()=>{
+    authService.getCurrentUser()
+    .then((userData)=>{
+      if(userData){
+        dispatch(login({userData}))
+      }else{
+        dispatch(logout())
+      }
+    })
+    .finally(()=>{
+      setLoading(false);
+    })
+  },[])
+
+  return !loading ? (<>
+    <div className='w-full'>
+      <Header/>
+      <main>
+        <div>Content</div>
+      </main>
+      <Footer/>
+    </div>
+  </>) : (<div className='flex justify-center font-bold text-3xl'>Loading...</div>)
 }
 
 export default App
