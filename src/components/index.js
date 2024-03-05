ef274a3a2858e1d57355e4ff0b3384fbd69e85ed
Akshat Gupta <guptaakshat370@gmai.com>
2024-03-05 15:22:36 +0530

    fix: imported all components

--- src/components/index.js
+++ src/components/index.js
@@ -3,12 +3,20 @@ import Footer from "./Footer/Footer";
 import Container from "./Container/Container"
 import Logo from "./Logo"
 import LogoutBtn from "./Header/LogoutBtn";
+import Button from "./Button/Button";
+import Input from "./Input/Input";
+import Select from "./Select/Select";
+import RTE from "./RTE/RTE";
 
 export { 
     Header, 
     Footer,
     Container,
     Logo,
-    LogoutBtn
+    LogoutBtn,
+    Button,
+    Input,
+    Select,
+    RTE
 };
 
